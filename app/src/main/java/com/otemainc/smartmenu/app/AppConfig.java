391f28dbe1f5012545de70f2d1922c77007b576b
TheOnlySmartBoy <o2jose43@gmail.com>
2017-12-16 14:41:23 +0300

    Begun performing User login

---
+++ app/src/main/java/com/otemainc/smartmenu/app/AppConfig.java
@@ -0,0 +1,14 @@
+package com.otemainc.smartmenu.app;
+
+/**
+ * Created by MASENO on 12/16/2017.
+ */
+
+
+    public class AppConfig {
+        // Server user login url
+        public static String URL_LOGIN = "http://192.168.200.250/SmartMenu/Login.php";
+
+        // Server user register url
+        public static String URL_REGISTER = "http://192.168.200.250/SmartMenu/Register.php";
+    }
